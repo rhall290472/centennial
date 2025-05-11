ec4b7620b40b965153686b7dfcd02284f737ae34
Richard Hall <rhall290472@gmail.com>
2025-05-11 13:19:16 -0600

    removed comments

--- sites/advancement/public/index.php
+++ sites/advancement/public/index.php
@@ -98,9 +98,6 @@
   <!-- Footer-->
   <?php load_template('/src/Templates/Footer.php'); ?>
 
-  <!-- Bootstrap core JS-->
-  <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script> -->
   <!-- Core theme JS-->
   <script src="js/scripts.js"></script>
 </body>
