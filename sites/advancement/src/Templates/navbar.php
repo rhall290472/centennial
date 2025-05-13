0f9f4ec4fb4c712a9c540b118bc0b69bbbafc87c
Richard Hall <rhall290472@gmail.com>
2025-05-13 14:53:57 -0600

    Moved code to include files

--- sites/advancement/src/Templates/navbar.php
+++ sites/advancement/src/Templates/navbar.php
@@ -1,99 +1,56 @@
-<?php
-
-/**
- * File: navbar.php
- * Description: Responsive navigation bar for Centennial District Merit Badges
- * Author: Richard Hall
- * License: Proprietary Software, Copyright 2024 Richard Hall
- */
-
-defined('IN_APP') or die('Direct access not allowed.');
-
-// Start session
-if (session_status() === PHP_SESSION_NONE) {
-  session_start([
-    'cookie_httponly' => true,
-    'use_strict_mode' => true,
-    'cookie_secure' => isset($_SERVER['HTTPS'])
-  ]);
-}
-
-// Generate CSRF token for Log off
-if (empty($_SESSION['csrf_token'])) {
-  $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
-}
-
-// Navigation links
-$nav_links = NAV_LINKS; // Defined in config.php
-$current_page = basename($_SERVER['PHP_SELF']);
-
-// Set active state
-foreach ($nav_links as &$link) {
-  $link['active'] = (basename($link['href']) === $current_page);
-}
-
-//TODO: Need to move to different folder
-$path_logon = SITE_URL . '/centennial/sites/advancement/src/Pages/logon.php';
-$path_logoff = SITE_URL . '/centennial/sites/advancement/src/Pages/logoff.php';
-
-// Add Log on/Log off link
-if (!empty($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
-  $nav_links[] = [
-    'href' => $path_logoff . '?csrf=' . urlencode($_SESSION['csrf_token']),
-    'text' => '<i class="bi bi-box-arrow-right"></i> Log off',
-    'active' => false,
-    'rel' => 'nofollow',
-    'aria-label' => 'Log off from your account'
-  ];
-} else {
-  $nav_links[] = [
-    'href' => $path_logon . '?csrf=' . urlencode($_SESSION['csrf_token']),
-    'text' => '<i class="bi bi-person"></i> Log on',
-    'active' => ($current_page === 'logon.php'),
-    'rel' => 'nofollow',
-    'aria-label' => 'Log in to your account'
-  ];
-}
-
-// Create a new array with updated active states
-$updated_nav_links = array_map(function ($link) use ($current_page) {
-  $link['active'] = (basename($link['href']) === $current_page);
-  return $link;
-}, $nav_links);
-
-
-
-// Helper function to render nav item
-function render_nav_item($link)
-{
-  $active_class = $link['active'] ? ' active' : '';
-  $aria_current = $link['active'] ? ' aria-current="page"' : '';
-  $rel_attr = !empty($link['rel']) ? ' rel="' . $link['rel'] . '"' : '';
-  $aria_label = !empty($link['aria-label']) ? ' aria-label="' . $link['aria-label'] . '"' : '';
-  return '<li class="nav-item"><a class="nav-link' . $active_class . '" href="' . $link['href'] . '"' .
-    $aria_current . $rel_attr . $aria_label . '>' . $link['text'] . '</a></li>';
-}
-?>
-
-
-
-
-
-
-
-<!-- Responsive navbar -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark" role="navigation" aria-label="Main navigation">
-  <div class="container px-4 px-lg-5">
-    <a class="navbar-brand" href="<?php echo SITE_URL; ?>/"><?php echo PAGE_TITLE; ?></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="?page=home">Centennial District Advancement</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <?php foreach ($updated_nav_links as $link): ?>
-          <?php echo render_nav_item($link); ?>
-        <?php endforeach; ?>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link <?php echo $page === 'home' ? 'active' : ''; ?>" href="?page=home">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle <?php echo in_array($page, ['pack-summary', 'pack-below-goal', 'pack-meeting-goal']) ? 'active' : ''; ?>" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Packs
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="?page=pack-summary">Summary</a></li>
+            <li><a class="dropdown-item" href="?page=pack-below-goal">Below District Goal</a></li>
+            <li><a class="dropdown-item" href="?page=pack-meeting-goal">Meeting District Goal</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle <?php echo in_array($page, ['troop-summary', 'troop-below-goal', 'troop-meeting-goal']) ? 'active' : ''; ?>" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Troops
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="?page=troop-summary">Summary</a></li>
+            <li><a class="dropdown-item" href="?page=troop-below-goal">Below District Goal</a></li>
+            <li><a class="dropdown-item" href="?page=troop-meeting-goal">Meeting District Goal</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link <?php echo $page === 'crew-summary' ? 'active' : ''; ?>" href="?page=crew-summary">Crews</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle <?php echo in_array($page, ['adv-report', 'membership-report']) ? 'active' : ''; ?>" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Reports
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="?page=adv-report">Advancement Report</a></li>
+            <li><a class="dropdown-item" href="?page=membership-report">Membership Report</a></li>
+          </ul>
+        </li>
+        <?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true): ?>
+          <li class="nav-item">
+            <a class="nav-link" href="?page=logout">Logout</a>
+          </li>
+        <?php else: ?>
+          <li class="nav-item">
+            <a class="nav-link <?php echo $page === 'login' ? 'active' : ''; ?>" href="?page=login">Login</a>
+          </li>
+        <?php endif; ?>
       </ul>
     </div>
   </div>
--- sites/advancement/src/Templates/sidebar.php
+++ sites/advancement/src/Templates/sidebar.php
@@ -1,157 +1,86 @@
-<?php
-defined('IN_APP') or die('Direct access not allowed.');
-
-?>
-<aside class="col-auto col-md-3 col-xl-auto px-sm-2 px-0 bg-dark  d-print-none">
-  <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-    <p class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-      <span class="fs-5 d-none d-sm-inline">Menu</span>
-    </p>
-    <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-      <!-- Dropdown with Submenu -->
+<!-- Sidebar -->
+<div class="sidebar d-flex flex-column flex-shrink-0 p-3 bg-light" id="sidebar">
+  <a href="?page=home" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+    <span class="fs-4">Menu</span>
+  </a>
+  <hr>
+  <ul class="nav nav-pills flex-column mb-auto">
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <i class="fs-4 bi-person"></i><span class="ms-1 d-none d-sm-inline">Adults</span>
+      </a>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="?page=untrained">Untrained Leaders</a></li>
+        <li><a class="dropdown-item" href="?page=ypt">Expired YPT</a></li>
+      </ul>
+    </li>
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <i class="fs-4 bi-emoji-surprise"></i><span class="ms-1 d-none d-sm-inline">Packs</span>
+      </a>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="?page=pack-summary">Summary</a></li>
+        <li><a class="dropdown-item" href="?page=pack-below-goal">Below District Goal</a></li>
+        <li><a class="dropdown-item" href="?page=pack-meeting-goal">Meeting District Goal</a></li>
+      </ul>
+    </li>
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <i class="fs-4 bi-backpack4"></i><span class="ms-1 d-none d-sm-inline">Troops</span>
+      </a>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="?page=troop-summary">Summary</a></li>
+        <li><a class="dropdown-item" href="?page=troop-below-goal">Below District Goal</a></li>
+        <li><a class="dropdown-item" href="?page=troop-meeting-goal">Meeting District Goal</a></li>
+      </ul>
+    </li>
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <i class="fs-4 bi-shield"></i><span class="ms-1 d-none d-sm-inline">Crews</span>
+      </a>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="?page=crew-summary">Summary</a></li>
+      </ul>
+    </li>
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <i class="fs-4 bi-clipboard2-data"></i><span class="ms-1 d-none d-sm-inline">Reports</span>
+      </a>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="?page=adv-report">District Advancement Report</a></li>
+        <li><a class="dropdown-item" href="?page=membership-report">Membership</a></li>
+      </ul>
+    </li>
+    <?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true): ?>
+      <li class="nav-item">
+        <a class="nav-link" href="?page=logout">Logout</a>
+      </li>
+    <?php else: ?>
+      <li class="nav-item">
+        <a class="nav-link <?php echo $page === 'login' ? 'active' : ''; ?>" href="?page=login">Login</a>
+      </li>
+    <?php endif; ?>
+    <?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true): ?>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" role="button"
-          data-bs-toggle="dropdown" aria-expanded="false">
-          <i class="fs-4 bi-person text-white-50"></i><span class="ms-1 d-none d-sm-inline text-white-50">Adults</span>
+        <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <i class="fs-4 bi-backpack4"></i><span class="ms-1 d-none d-sm-inline text-danger">Admin</span>
         </a>
         <ul class="dropdown-menu">
-          <li><a href="../src/Pages/Untrained.php?MemberID=&btn=ByLastName" class=" dropdown-item">Untrained Leaders</a></li>
-          <li><a href="../src/Pages/YPT.php?MemberID=&btn=ByLastName" class=" dropdown-item">Expired YPT</a></li>
+          <li><a class="dropdown-item" href="?page=updatedata&update=TrainedLeader">Training</a></li>
+          <li><a class="dropdown-item" href="?page=updatedata&update=Updateypt">YPT</a></li>
+          <li><a class="dropdown-item" href="?page=updatedata&update=UpdateTotals">Upload COR Data</a></li>
+          <li><a class="dropdown-item" href="?page=updatedata&update=UpdateFunctionalRole">Functional Roles</a></li>
+          <li><a class="dropdown-item" href="?page=updatedata&update=UpdateCommissioners">Pack Assigned Commissioners</a></li>
+          <li><a class="dropdown-item" href="?page=updatedata&update=UpdatePack">Pack Advancements</a></li>
+          <li><a class="dropdown-item" href="?page=updatedata&update=UpdateAdventure">Pack Awards</a></li>
+          <li><a class="dropdown-item" href="?page=updatedata&update=UpdateTroop">Troop Advancements</a></li>
+          <li><a class="dropdown-item" href="?page=updatedata&update=UpdateCrew">Crew Advancements</a></li>
+          <li><a class="dropdown-item" href="?page=updatedata&update=UpdateVenturing">Venturing</a></li>
         </ul>
       </li>
-
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" role="button"
-          data-bs-toggle="dropdown" aria-expanded="false">
-          <i class="fs-4 bi-emoji-surprise text-white-50"></i><span class="ms-1 d-none d-sm-inline text-white-50">Packs</span>
-          <!-- <img src="./images/cub-scout.ico" alt="Cub Scouts" class="cubscout-icon text-white-50"><span class="ms-1 d-none d-sm-inline text-white-50">Packs</span> -->
-        </a>
-        <ul class="dropdown-menu">
-          <!-- <li><a href="./pack_index.php" class="dropdown-item text-danger">Index</a></li> -->
-          <li><a href="../src/Pages/pack_summary.php" class="dropdown-item">Summary</a></li>
-          <li><a href="<?php echo SITE_URL . '/centennial/sites/advancement/src/Pages/pack_below_goal.php'; ?>" class="dropdown-item">Below District Goal's</a></li>
-          <li><a href="<?php echo SITE_URL . '/centennial/sites/advancement/src/Pages/pack_meeting_goal.php'; ?>" class="dropdown-item">Meeting District Goal's</a></li>
-        </ul>
-      </li>
-
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" role="button"
-          data-bs-toggle="dropdown" aria-expanded="false">
-          <i class="fs-4 bi-backpack4 text-white-50"></i><span class="ms-1 d-none d-sm-inline text-white-50">Troops</span>
-        </a>
-        <ul class="dropdown-menu">
-          <li><a href="<?php echo SITE_URL . '/centennial/sites/advancement/src/Pages/troop_summary.php'; ?>" class="dropdown-item">Summary</a></li>
-          <li><a href="<?php echo SITE_URL . '/centennial/sites/advancement/src/Pages/troop_below_goal.php'; ?>" class="dropdown-item">Below District Goal's</a></li>
-          <li><a href="<?php echo SITE_URL . '/centennial/sites/advancement/src/Pages/troop_meeting_goal.php'; ?>" class="dropdown-item">Meeting District Goal's</a></li>
-        </ul>
-      </li>
-
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" role="button"
-          data-bs-toggle="dropdown" aria-expanded="false">
-          <i class="fs-4 bi-shield text-white-50"></i><span class="ms-1 d-none d-sm-inline text-white-50">Crews</span>
-        </a>
-        <ul class="dropdown-menu">
-          <li><a href="<?php echo SITE_URL . '/centennial/sites/advancement/src/Pages/crew_summary.php'; ?>" class="dropdown-item">Summary</a></li>
-        </ul>
-      </li>
-
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" role="button"
-          data-bs-toggle="dropdown" aria-expanded="false">
-          <i class="fs-4 bi-clipboard2-data text-white-50"></i><span class="ms-1 d-none d-sm-inline text-white-50">Reports</span>
-        </a>
-        <ul class="dropdown-menu">
-          <li><a href="<?php echo SITE_URL . '/centennial/sites/advancement/src/Pages/adv_report.php'; ?>" class="dropdown-item">District Advancement Report</a></li>
-          <li><a href="<?php echo SITE_URL . '/centennial/sites/advancement/src/Pages/membership_report.php'; ?>" class="dropdown-item">Membership</a></li>
-        </ul>
-      </li>
-
-      <?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) { ?>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" role="button"
-            data-bs-toggle="dropdown" aria-expanded="false">
-            <i class="fs-4 bi-book text-danger"></i><span class="ms-1 d-none d-sm-inline text-danger">Admin</span>
-          </a>
-          <ul class="dropdown-menu">
-            <li class="submenu submenu-md dropend">
-              <a class="dropdown-item dropdown-toggle" role="button"
-                data-bs-toggle="dropdown" aria-expanded="false"
-                onclick="event.stopPropagation();">
-                Upload Adult Data
-              </a>
-              <ul class="dropdown-menu">
-                <li><a href="<?php echo SITE_URL . '/centennial/sites/advancement/src/Pages/UpdateData.php?Update=TrainedLeader'; ?>" class=" dropdown-item">Training</a></li>
-                <li><a href="<?php echo SITE_URL . '/centennial/sites/advancement/src/Pages/UpdateData.php?Update=Updateypt'; ?>" class=" dropdown-item">YPT</a></li>
-              </ul>
-            </li>
-
-            <li class="submenu submenu-md dropend">
-              <a class="dropdown-item dropdown-toggle" role="button"
-                data-bs-toggle="dropdown" aria-expanded="false"
-                onclick="event.stopPropagation();">
-                Upload Membership Data
-              </a>
-              <ul class="dropdown-menu">
-                <li><a href="<?php echo SITE_URL . '/centennial/sites/advancement/src/Pages/UpdateData.php?Update=UpdateTotals'; ?>" class=" dropdown-item">Upload COR Data</a></li>
-                <li><a href="<?php echo SITE_URL . '/centennial/sites/advancement/src/Pages/UpdateData.php?Update=FunctionalRole'; ?>" class=" dropdown-item">Functional Roles</a></li>
-                <li><a href="<?php echo SITE_URL . '/centennial/sites/advancement/src/Pages/UpdateData.php?Update=UpdateCommissioners'; ?>" class=" dropdown-item">Assigned Commissioners</a></li>
-              </ul>
-            </li>
-
-            <li class="submenu submenu-md dropend">
-              <a class="dropdown-item dropdown-toggle" role="button"
-                data-bs-toggle="dropdown" aria-expanded="false"
-                onclick="event.stopPropagation();">
-                Upload Pack Data
-              </a>
-              <ul class="dropdown-menu">
-                <li><a href="<?php echo SITE_URL . '/centennial/sites/advancement/src/Pages/UpdateData.php?Update=UpdatePack'; ?>" class=" dropdown-item">Advancements</a></li>
-                <li><a href="<?php echo SITE_URL . '/centennial/sites/advancement/src/Pages/UpdateData.php?Update=UpdateAdventure'; ?>" class=" dropdown-item">Awards</a></li>
-              </ul>
-            </li>
-
-            <li class="submenu submenu-md dropend">
-              <a class="dropdown-item dropdown-toggle" role="button"
-                data-bs-toggle="dropdown" aria-expanded="false"
-                onclick="event.stopPropagation();">
-                Upload Troop Data
-              </a>
-              <ul class="dropdown-menu">
-                <li><a href="<?php echo SITE_URL . '/centennial/sites/advancement/src/Pages/UpdateData.php?Update=UpdateTroop'; ?>" class=" dropdown-item">Advancements</a></li>
-              </ul>
-            </li>
-
-            <li class="submenu submenu-md dropend">
-              <a class="dropdown-item dropdown-toggle" role="button"
-                data-bs-toggle="dropdown" aria-expanded="false"
-                onclick="event.stopPropagation();">
-                Upload Crew Data
-              </a>
-              <ul class="dropdown-menu">
-                <li><a href="#!" class=" dropdown-item">Advancements</a></li>
-                <li><a href="<?php echo SITE_URL . '/centennial/sites/advancement/src/Pages/UpdateData.php?Update=UpdateVenturing'; ?>" class=" dropdown-item">Venturing</a></li>
-              </ul>
-            </li>
-            <li><a href="<?php echo SITE_URL . '/centennial/sites/advancement/src/Pages/ErrorLog.php'; ?>" class=" dropdown-item">View Error Log</a></li>
-          </ul>
-        </li>
-      <?php } ?>
-    </ul>
-  </div>
-</aside>
-
-<!-- Bootstrap core JS-->
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-
-
-<!-- Bootstrap JS and Popper.js -->
-<!-- jQuery (required for Bootstrap's JavaScript plugins) -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-<!-- Popper.js (required for Bootstrap dropdowns) -->
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js"></script> -->
-
-<!-- Bootstrap's JavaScript -->
-<!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> -->
+    <?php endif; ?>
+  </ul>
+  <button class="btn btn-outline-secondary mt-3 d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#sidebar" aria-expanded="false" aria-controls="sidebar">
+    Close Sidebar
+  </button>
+</div>
