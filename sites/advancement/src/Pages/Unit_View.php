1795571ccf85c8e8cbe7f92d3fb77790c80970b4
rhall290472 <56454660+rhall290472@users.noreply.github.com>
2025-09-08 05:39:21 -0600

    Correct location of the CUnit.php file

--- sites/advancement/src/Pages/Unit_View.php
+++ sites/advancement/src/Pages/Unit_View.php
@@ -33,7 +33,7 @@ if (file_exists(__DIR__ . '/../../config/config.php')) {
 }
 
 load_class(SHARED_PATH . '/src/Classes/CAdvancement.php');
-load_class(BASE_PATH . '/src/Classes/CUnit.php');
+load_class(SHARED_PATH . '/src/Classes/CUnit.php');
 load_class(SHARED_PATH . 'src/Classes/cAdultLeaders.php');
 
 
