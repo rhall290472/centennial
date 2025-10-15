7efa8eebee8b796a3a0eea9f99ef45a2ae6000ec
rhall290472 <56454660+rhall290472@users.noreply.github.com>
2025-10-15 06:47:00 -0600

    Add sort table and export function to page

--- sites/districtawards/src/Templates/header.php
+++ sites/districtawards/src/Templates/header.php
@@ -1,36 +1,71 @@
-	    <!-- Google tag (gtag.js) -->
-	    <script async src="https://www.googletagmanager.com/gtag/js?id=G-6PCWFTPZDZ"></script>
-	    <script>
-	    	window.dataLayer = window.dataLayer || [];
+<?php
+/*
+!==============================================================================!
+!\                                                                            /!
+!\\                                                                          //!
+! \##########################################################################/ !
+!  #         This is Proprietary Software of Richard Hall                   #  !
+!  ##########################################################################  !
+!  ##########################################################################  !
+!  #                                                                        #  !
+!  #                                                                        #  !
+!  #   Copyright 2017-2024 - Richard Hall                                   #  !
+!  #                                                                        #  !
+!  #   The information contained herein is the property of Richard          #  !
+!  #   Hall, and shall not be copied, in whole or in part, or               #  !
+!  #   disclosed to others in any manner without the express written        #  !
+!  #   authorization of Richard Hall.                                       #  !
+!  #                                                                        #  !
+!  #                                                                        #  !
+! /##########################################################################\ !
+!//                                                                          \\!
+!/                                                                            \!
+!==============================================================================!
+*/
+?>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6PCWFTPZDZ"></script>
+<script>
+	window.dataLayer = window.dataLayer || [];
 
-	    	function gtag() {
-	    		dataLayer.push(arguments);
-	    	}
-	    	gtag('js', new Date());
-	    	gtag('config', 'G-6PCWFTPZDZ');
-	    </script>
+	function gtag() {
+		dataLayer.push(arguments);
+	}
+	gtag('js', new Date());
+	gtag('config', 'G-6PCWFTPZDZ');
+</script>
 
-	    <meta charset="utf-8" />
-	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-	    <meta name="description" content="Centennial District Awards." />
-	    <meta name="author" content="Richard Hall" />
-	    <title>Centennial District Awards</title>
-	    <!-- Favicon-->
-	    <link rel="icon" type="image/x-icon" href="https://shared.centennialdistrict.co/assets/centennial.ico" />
-	    <!-- Bootstrap icons-->
-	    <!-- Vendor CSS Files -->
-	    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css">
-	    <!-- <link href="assets/vendor/bootstrap/css/bootstrap.css" rel="stylesheet" /> -->
-	    <!-- <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet" /> -->
-	    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
-	    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" /> -->
-	    <!-- Core theme CSS (includes Bootstrap)-->
-	    <?php
-			$is_localhost = isset($_SERVER['SERVER_NAME']) && in_array($_SERVER['SERVER_NAME'], ['localhost', '127.0.0.1']);
-			if ($is_localhost) {
-			?>
-	    	<link href="https://localhost/centennial/shared/assets/styles.css" rel="stylesheet" />
-	    <?php
-			} else { ?>
-	    	<link href="https://shared.centennialdistrict.co/assets/styles.css" rel="stylesheet" />
-	    <?php } ?>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+<meta name="description" content="Centennial District Awards." />
+<meta name="author" content="Richard Hall" />
+<title>Centennial District Awards</title>
+<!-- Favicon-->
+<link rel="icon" type="image/x-icon" href="https://shared.centennialdistrict.co/assets/centennial.ico" />
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css">
+<!-- Bootstrap Icons -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+<!-- DataTables and Buttons CSS -->
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.bootstrap4.min.css">
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<!-- DataTables and Buttons JS -->
+<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap4.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.bootstrap4.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
+<!-- Core theme CSS -->
+<?php
+$is_localhost = isset($_SERVER['SERVER_NAME']) && in_array($_SERVER['SERVER_NAME'], ['localhost', '127.0.0.1']);
+if ($is_localhost) {
+?>
+	<link href="https://localhost/centennial/shared/assets/styles.css" rel="stylesheet" />
+<?php } else { ?>
+	<link href="https://shared.centennialdistrict.co/assets/styles.css" rel="stylesheet" />
+<?php } ?>
--- sites/districtawards/src/pages/ReportAwardYear.php
+++ sites/districtawards/src/pages/ReportAwardYear.php
@@ -25,99 +25,188 @@
 
 load_class(BASE_PATH . '/src/Classes/CDistrictAwards.php');
 $cDistrictAwards = cDistrictAwards::getInstance();
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta name="description" content="ReportAwardYear.php">
+  <meta name="description" content="ReportAwardYear.php">
+  <style>
+    .fixed_header th,
+    .fixed_header td {
+      padding: 8px;
+      text-align: left;
+    }
+
+    .dataTables_wrapper {
+      margin: 20px;
+    }
+
+    /* Style for DataTables button container */
+    .dt-buttons {
+      margin-bottom: 10px;
+      text-align: center;
+    }
+
+    .dt-buttons .btn {
+      margin-right: 5px;
+    }
+  </style>
 </head>
 
 <body>
+  <?php
+  if (isset($_POST['SubmitYear'])) {
+    $year = $_POST['Year'];
+    $cDistrictAwards->SetYear($year);
+  }
+  $cDistrictAwards->SelectYear();
 
-    <?php
-    $csv_hdr = "Year, First Name, Preferred Name, Last Name, Award, Status, Member ID, Unit";
-    $csv_output = "";
+  // Set PDF filename and header based on selected year
+  $year = $cDistrictAwards->GetYear();
+  $pdf_filename = $year == "" ? "Nominees_All_Years" : "Nominees_" . $year;
+  $pdf_header = $year == "" ? "Centennial District Awards - All Years" : "Centennial District Awards - " . $year;
+  ?>
 
-    if (isset($_POST['SubmitYear'])) {
-        $year = $_POST['Year'];
-        $cDistrictAwards->SetYear($year);
-        //header('Refresh: ' . 1);
-    }
-    // Dispay the Year Select dropdown selection
-    $cDistrictAwards->SelectYear();
-    ?>
+  <center>
+    <h4><?php
+        $msg = $year == "" ? "All Years" : $cDistrictAwards->GetYear();
+        echo "Nominees for " . $msg
+        ?> </h4>
+    <table id="nomineesTable" class="fixed_header table table-striped table-bordered" style="width:100%">
+      <thead>
+        <tr>
+          <th style='width:150px'>Year</th>
+          <th style='width:150px'>First Name</th>
+          <th style='width:150px'>Preferred Name</th>
+          <th style='width:150px'>Last Name</th>
+          <th style='width:500px'>Award</th>
+          <th style='width:50px'>Status</th>
+          <th style='width:150px'>Member ID</th>
+          <th style='width:500px'>Unit</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        if ($year == "")
+          $queryNominees = "SELECT * FROM `district_awards` WHERE (`IsDeleted` IS NULL || `IsDeleted` <>'1') ORDER BY `Award`";
+        else
+          $queryNominees = "SELECT * FROM `district_awards` WHERE `Year`='$year' AND (`IsDeleted` IS NULL || `IsDeleted` <>'1') ORDER BY `Award`, `Year` DESC";
 
-    <center>
+        if (!$ResultNominees = $cDistrictAwards->doQuery($queryNominees)) {
+          $msg = "Error: doQuery()";
+          $cDistrictAwards->function_alert($msg);
+        }
 
-        <h4><?php
-            $year = $cDistrictAwards->GetYear();
-            $msg = $year == "" ? "All Years" : $cDistrictAwards->GetYear();
-            echo "Nominees for " . $msg
-            ?> </h4>
-        <table>
-            <thead>
-                <tr>
-                    <th style='width:150px'>Year</th>
-                    <th style='width:150px'>First Name</th>
-                    <th style='width:150px'>Perferred Name</th>
-                    <th style='width:150px'>Last Name</th>
-                    <th style='width:500px'>Award</th>
-                    <th style='width:50px'>Status</th>
-                    <th style='width:150px'>Member ID</th>
-                    <th style='width:500px'>Unit</th>
-                </tr>
-            </thead>
-            <?php
-            if ($year == "")
-                $queryNominees = "SELECT * FROM `district_awards` WHERE (`IsDeleted` IS NULL || `IsDeleted` <>'1') ORDER BY `Award`";
-            else
-                $queryNominees = "SELECT * FROM `district_awards` WHERE `Year`='$year' AND (`IsDeleted` IS NULL || `IsDeleted` <>'1') ORDER BY `Award`, `Year` DESC";
+        while ($rowNominee = $ResultNominees->fetch_assoc()) {
+          if ($rowNominee['NomineeIDX'] != -1) {
+            $AwardName = $cDistrictAwards->GetAwardName($rowNominee['Award']);
+            $Status = $cDistrictAwards->GetAwardStatus($rowNominee['Status']);
+            echo "<tr><td style='width:150px'>" .
+              $rowNominee["Year"] . "</td><td style='width:150px'>" .
+              $rowNominee["FirstName"] . "</td><td style='width:150px'>" .
+              $rowNominee["PName"] . "</td><td style='width:150px'>" .
+              "<a href=index.php?page=edit-nominee&NomineeIDX=" . $rowNominee['NomineeIDX'] . ">" . $rowNominee['LastName'] . "</a></td><td style='width:500px'>" .
+              $AwardName . "</td><td style='width:50px'>" .
+              $Status . "</td><td style='width:150px'>" .
+              $rowNominee['MemberID'] . "</td><td style='width:500px'>" .
+              $rowNominee['Unit'] . "</td></tr>";
+          }
+        }
+        ?>
+      </tbody>
+    </table>
+  </center>
 
-            if (!$ResultNominees = $cDistrictAwards->doQuery($queryNominees)) {
-                $msg = "Error: doQuery()";
-                $cDistrictAwards->function_alert($msg);
+  <script>
+    $(document).ready(function() {
+      $('#nomineesTable').DataTable({
+        dom: 'Bfrtip',
+        paging: false, // Disable pagination to remove Previous/Next buttons
+        buttons: [{
+            extend: 'copy',
+            className: 'btn btn-primary btn-sm d-print-none mt-2',
+            text: 'Copy to Clipboard',
+            exportOptions: {
+              columns: ':visible'
             }
-
-            ?><tbody>
-                <?php
-                while ($rowNominee = $ResultNominees->fetch_assoc()) {
-                    if ($rowNominee['NomineeIDX'] != -1) {
-                        $AwardName = $cDistrictAwards->GetAwardName($rowNominee['Award']);
-                        $Status = $cDistrictAwards->GetAwardStatus($rowNominee['Status']);
-
-                        echo "<tr><td style='width:150px'>" .
-                            $rowNominee["Year"] . "</td><td style='width:150px'>" .
-                            $rowNominee["FirstName"] . "</td><td style='width:150px'>" .
-                            $rowNominee["PName"] . "</td><td style='width:150px'>" .
-                            "<a href=index.php?page=edit-nominee&NomineeIDX=" . $rowNominee['NomineeIDX'] . ">" . $rowNominee['LastName'] . "</a> </td><td  style='width:500px'>" .
-                            $AwardName . "</td><td style='width:50px'>" .
-                            $Status . "</td><td style='width:150px'>" .
-                            $rowNominee['MemberID'] . "</td><td style='width:500px'>" .
-                            $rowNominee['Unit'] . "</td></tr>";
-
-                        $csv_output .= $rowNominee["Year"] . ",";
-                        $csv_output .= $rowNominee["FirstName"] . ",";
-                        $csv_output .= $rowNominee["PName"] . ", ";
-                        $csv_output .= $rowNominee["LastName"] . ",";
-                        $csv_output .= $AwardName . ",";
-                        $csv_output .= $Status . ",";
-                        $csv_output .= $rowNominee['MemberID'] . ",";
-                        $csv_output .= $rowNominee['Unit'] . "\n";
-                    }
+          },
+          {
+            extend: 'csv',
+            className: 'btn btn-primary btn-sm d-print-none mt-2',
+            text: 'Export CSV',
+            exportOptions: {
+              columns: ':visible'
+            }
+          },
+          {
+            extend: 'excel',
+            className: 'btn btn-primary btn-sm d-print-none mt-2',
+            text: 'Export Excel',
+            exportOptions: {
+              columns: ':visible'
+            }
+          },
+          {
+            extend: 'pdf',
+            className: 'btn btn-primary btn-sm d-print-none mt-2',
+            text: 'Export PDF',
+            filename: '<?php echo $pdf_filename; ?>', // Custom PDF filename
+            orientation: 'landscape', // Maintain landscape mode
+            exportOptions: {
+              columns: ':visible'
+            },
+            customize: function(doc) {
+              // Add header to every page
+              doc.header = {
+                text: '<?php echo $pdf_header; ?>',
+                alignment: 'center',
+                margin: [0, 10, 0, 10],
+                fontSize: 12
+              };
+              // Set page margins to maximize table width
+              doc.pageMargins = [20, 30, 20, 20]; // Increased top margin to accommodate header
+              // Adjust column widths to fit within one page (proportional to content)
+              doc.content[1].table.widths = ['auto', 'auto', 'auto', 'auto', '*', 'auto', 'auto', '*'];
+              // Set font sizes for better readability
+              doc.styles.tableHeader.fontSize = 8;
+              doc.styles.tableBodyOdd.fontSize = 7;
+              doc.styles.tableBodyEven.fontSize = 7;
+              // Ensure table fits page width
+              doc.content[1].table.layout = {
+                hLineWidth: function(i, node) {
+                  return 0.5;
+                },
+                vLineWidth: function(i, node) {
+                  return 0.5;
+                },
+                paddingLeft: function(i, node) {
+                  return 2;
+                },
+                paddingRight: function(i, node) {
+                  return 2;
+                },
+                paddingTop: function(i, node) {
+                  return 2;
+                },
+                paddingBottom: function(i, node) {
+                  return 2;
                 }
-                echo "</tbody>";
-                echo "</table>";
-                ?>
-                <form name="export" action="../export.php" method="post" style="padding: 20px;">
-                    <input class='btn btn-primary btn-sm' style="width:220px" type="submit" value="Export table to CSV">
-                    <input type="hidden" value="<?php echo $csv_hdr; ?>" name="csv_hdr">
-                    <input type="hidden" value="<?php echo $csv_output; ?>" name="csv_output">
-                </form>
-    </center>
-
+              };
+            }
+          }
+        ],
+        order: [
+          [0, 'desc']
+        ], // Default sort by Year descending
+        columnDefs: [{
+            type: 'string',
+            targets: '_all'
+          } // Ensure proper string sorting
+        ]
+      });
+    });
+  </script>
 </body>
 
 </html>
