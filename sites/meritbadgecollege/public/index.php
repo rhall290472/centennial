58915ea24ac28ddba3a77a5130179c5b7a868105
rhall290472 <56454660+rhall290472@users.noreply.github.com>
2026-02-20 14:06:59 -0700

    Added content to the index page.

--- sites/meritbadgecollege/public/index.php
+++ sites/meritbadgecollege/public/index.php
@@ -252,6 +252,68 @@ get_csrf_token();
               <p>Once you select your name from the counselors drop down, only the Merit badges that you are approved for will be shown</p>
 
 
+              <p><a href="https://filestore.scouting.org/filestore/pdf/512-065.pdf">A Guide for Merit Badge Counseling</a></p>
+              <p><a href="https://www.scouting.org/skills/merit-badges/">BSA Merit Badge Hub</a></p>
+              <p><b>Some thoughts from the <a href="https://filestore.scouting.org/filestore/pdf/33088.pdf?_gl=1*1qopu25*_ga*MjAxOTIzMzAzNS4xNjIxOTg3NTcy*_ga_20G0JHESG4*MTYyMTk4NzU3Mi4xLjEuMTYyMTk4NzcwNC4yMw..">Guide to Advancment</a></b></p>
+
+
+
+              <p>&nbsp;</p>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3856.1779803017257!2d-104.80239407026473!3d39.71812148920198!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sus!4v1731870730623!5m2!1sen!2sus" width="800" height="650" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+
+              <h4>7.0.3.2 Group Instruction</h4>
+              <p>It is acceptable—and sometimes desirable—for merit badges to be taught in group settings. This often occurs
+                at camp and merit badge midways, fairs,
+                clinics, or similar events, and even online through webinars. These can be efficient methods, and
+                interactive group discussions can support learning.
+                Group instruction can also be attractive to “guest experts” assisting registered and approved counselors.
+                Slide shows, skits, demonstrations, panels,
+                and various other techniques can also be employed, but as any teacher can attest, not everyone will learn
+                all the material. Because of the importance of
+                individual attention and personal learning in the merit badge program, group instruction should be focused
+                on those scenarios where the benefits are
+                compelling. There must be attention to each individual’s projects and fulfillment of all requirements. We
+                must know that every Scout—actually and
+                personally—completed them. If, for example, a requirement uses words like “show,” “demonstrate,” or
+                “discuss,” then every Scout must do that. It is
+                unacceptable to award badges on the basis of sitting in classrooms watching demonstrations, or remaining
+                silent during discussions. It is sometimes reported
+                that Scouts who have received merit badges through group instructional settings have not fulfilled all the
+                requirements. To offer a quality merit badge program,
+                council and district advancement committees should ensure the following are in place for all group
+                instructional events. A culture is established for merit
+                badge group instructional events that partial completions are acceptable expected results. A guide or
+                information sheet is distributed in advance of events
+                that promotes the acceptability of partials, explains how merit badges can be finished after events, lists
+                merit badge prerequisites, and provides other helpful
+                information that will establish realistic expectations for the number of merit badges that can be earned at
+                an event.</p>
+              <p>Merit badge counselors are known to be registered and approved.</p>
+              <p>Any guest experts or guest speakers, or others assisting who are not registered and approved as merit badge
+                counselors, do not accept the responsibilities of,
+                or behave as, merit badge counselors, either at a group instructional event or at any other time. Their
+                service is temporary, not ongoing.</p>
+              <p>Counselors agree to sign off only requirements that Scouts have actually and personally completed.</p>
+              <p>Counselors agree not to assume that stated prerequisites for an event have been completed without some level
+                of evidence that the work has been done. Pictures
+                and letters from other merit badge counselors or unit leaders are the best form of prerequisite
+                documentation when the actual work done cannot be brought to the
+                camp or site of the merit badge event.</p>
+              <p>There is a mechanism for unit leaders or others to report concerns to a council advancement committee on
+                summer camp merit badge programs, group instructional
+                events, and any other merit badge counseling issues— especially in instances where it is believed BSA
+                procedures are not followed. See “Reporting Merit Badge
+                Counseling Concerns,” 11.1.0.0.</p>
+              <p>Additional guidelines and best practices can be found in the Merit Badge Group Instruction Guide, developed
+                by volunteers in conjunction with the National
+                Advancement Program Team. This guide for units, districts, and councils includes several important event
+                planning considerations as well as suggestions for
+                evaluating the event after it is over to identify opportunities for improvement. The guide can be downloaded
+                from
+                <a href="https://filestore.scouting.org/filestore/pdf/512-066_web.pdf">Merit Badge Group Instruction Guide</a>.
+              </p>
+
 
             </div>
           </div>
